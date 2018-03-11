5726f085a52b7d67a605438a18cf23a989306f33
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-03-12 00:40:24 +0100

    broken due to 'react-hot' changes

--- src_tutorial/pages/UserEdit.js
+++ src_tutorial/pages/UserEdit.js
@@ -1,11 +1,12 @@
 import React from"react";
 
-import {PageHeader} from 'react-bootstrap'
+import {Button, Col, Form, FormControl, FormGroup, Glyphicon, InputGroup, PageHeader} from 'react-bootstrap'
+import {Field, reduxForm} from "redux-form";
 
 /**
- * Homepage component
+ * USerEdit component
  */
-export default class UserEdit extends React.Component {
+class UserEdit extends React.Component {
 
   /**
    * Render
@@ -16,8 +17,58 @@ export default class UserEdit extends React.Component {
     return (
       <div>
         <PageHeader>User Add/Edit</PageHeader>
-        form
+        <Form horizontal>
+          <Field name="username" component={UserEdit.renderUsername}/>
+          <Field name="job" component={UserEdit.renderJob}/>
+          <FormGroup>
+            <Col smOffset={2} sm={2}>
+              <Button type="submit">Save</Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     )
   }
+
+  /**
+   * Render the field for username
+   * @param props
+   * @returns {*}
+   */
+  static renderUsername(props) {
+    return (
+      <FormGroup>
+        <Col sm={2}>Username</Col>
+        <Col sm={8}>
+          <FormControl {...props.input} id='username' type="text" placeholder="Username"/>
+        </Col>
+      </FormGroup>
+    )
+  }
+
+  /**
+   * Render the field for job
+   * @param props
+   * @returns {*}
+   */
+  static renderJob(props) {
+    return (
+      <FormGroup>
+        <Col sm={2}>Job</Col>
+        <Col sm={8}>
+          <InputGroup>
+            <FormControl {...props.input} id='job' type="text" placeholder="Job"/>
+            <InputGroup.Addon>
+              <Glyphicon glyph="briefcase"/>
+            </InputGroup.Addon>
+          </InputGroup>
+        </Col>
+      </FormGroup>
+    )
+  }
 }
+
+//decorate the form component
+export default UserEdit = reduxForm({
+  form: 'user_edit'
+})(UserEdit)
