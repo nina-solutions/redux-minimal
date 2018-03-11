97691c8eb17a0124bd68080c244dcd7416a5e771
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2018-03-11 22:18:27 +0100

    refactor into reducer class

--- src_tutorial/reducers/index.js
+++ src_tutorial/reducers/index.js
@@ -5,3 +5,28 @@ import {combineReducers} from "redux";
 export const reducers = combineReducers({
   users: users
 });
+
+export default function reducerCall(state, action, reducerClass) {
+  // get the action classs method
+  const [, method] = action.type.split('.');
+
+
+  const methods = Object.getOwnPropertyNames(reducerClass).filter(name => {
+    if (['name', 'length', 'prototype'].indexOf(name) === -1) {
+      return name;
+    }
+  });
+
+  // check if the action methods exists in this class
+  if (methods.find(x => x === method)) {;
+    const new_state = cloneObject(state);
+    return reducerClass[method](new_state, action);
+  }else {
+    return state;
+  }
+};
+
+function cloneObject(o) {
+  return JSON.parse(JSON.stringify(o));
+
+}
--- src_tutorial/reducers/users.js
+++ src_tutorial/reducers/users.js
@@ -1,35 +1,63 @@
+import reducerCall from './index'
 
-
+/**
+ * Reducer for users
+ * @param state
+ * @param action
+ * @returns {*}
+ */
 export default function users(state={}, action) {
-  let new_state;
-  switch (action.type) {
-    case 'users.modalDeleteShow':
-      new_state = JSON.parse(JSON.stringify(state));
-      new_state.modal = new_state.modal ? new_state.modal : {};
-      new_state.modal.list_delete = {
-        show: true,
-        id: action.id,
-        username: action.username
-      };
-      return new_state;
-    case 'users.modalDeleteHide':
-      new_state = JSON.parse(JSON.stringify(state));
-      new_state.modal.list_delete = {
-        show: false,
-        id: 0,
-        username: ''
-      };
-      return new_state;
-    case 'users.delete':
-      new_state = JSON.parse(JSON.stringify(state));
-      for (const i in new_state.list) {
-        if (new_state.list[i].id === action.id) {
-          new_state.list.splice(i, 1);
-          break;
-        }
+  return reducerCall(state, action, reducerClass);
+}
+
+/**
+ * reducer static class
+ */
+class reducerClass {
+  /**
+   * show the delete prompt
+   * @param new_state
+   * @param action
+   * @returns {*}
+   */
+  static modalDeleteShow(new_state, action) {
+    new_state.modal = new_state.modal ? new_state.modal : {};
+    new_state.modal.list_delete = {
+      show: true,
+      id: action.id,
+      username: action.username
+    };
+    return new_state;
+  }
+
+  /**
+   * hide the delete prompt
+   * @param new_state
+   * @param action
+   * @returns {*}
+   */
+  static modalDeleteHide(new_state, action) {
+    new_state.modal.list_delete = {
+      show: false,
+      id: 0,
+      username: ''
+    };
+    return new_state;
+  }
+
+  /**
+   * actually delete the user
+   * @param new_state
+   * @param action
+   * @returns {*}
+   */
+  static delete(new_state, action) {
+    for (const i in new_state.list) {
+      if (new_state.list[i].id === action.id) {
+        new_state.list.splice(i, 1);
+        break;
       }
-      return new_state;
-    default:
-      return state;
+    }
+    return new_state;
   }
 }
